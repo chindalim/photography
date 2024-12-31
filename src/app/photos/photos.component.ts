695a99cdbc6b1086ed3285a8a0640e13a48ea0f1
Chendar Lim <chendar.lim@live.com>
2024-12-31 11:06:44 -0800

    photo component added

---
+++ src/app/photos/photos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  imports: [],
+  templateUrl: './photos.component.html',
+  styleUrl: './photos.component.css'
+})
+export class PhotosComponent {
+  hero = 'Photo Main';
+
+}
